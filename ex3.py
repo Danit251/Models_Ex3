b468f4f4c49d8c161379720e27b443aa5e44ad58
danity <danity251@gmail.com>
2020-12-29 17:39:19 +0200

    finish em without optimization

--- ex3.py
+++ ex3.py
@@ -3,6 +3,13 @@ import sys
 from collections import Counter
 import numpy as np
 from math import e
+from datetime import datetime
+import logging
+from time import time
+
+now = datetime.now().strftime("%H_%M_%S")
+logging.basicConfig(filename=f"log_{now}", level=logging.INFO)
+logger = logging.getLogger()
 
 
 class Doc:
@@ -22,21 +29,22 @@ class Doc:
     def get_doc_len(self):
         return sum(self.words_stat.values())
 
+
 class Em:
     K = 10
     LAMBDA = 0.06
     EPSILON = 10**(-6)
 
     def __init__(self, path_data, path_topics):
+        logger.info("data loaded")
         self.data, self.words_stat = self.load_data(path_data)
+        logger.info("data loaded")
         self.i2topic, self.topic2i = self.load_topics(path_topics)
-
+        logger.info("topics loaded")
         self.num_topics = len(self.topic2i)
         self.num_docs = len(self.data)
         self.splited_data = self.initial_split(self.num_topics)
-        self.alpha = self.initialize_alpha(self.num_topics)
-        self.w = self.initialize_w(self.num_topics, self.num_docs)
-        print()
+        logger.info("init finished")
 
     def load_data(self, path):
         with open(path) as f:
@@ -64,18 +72,6 @@ class Em:
             splited_data[mod].append(self.data[i])
         return splited_data
 
-    def topic_prob(self, doc, topic):
-        return 0
-
-    def get_distribution(self, doc):
-        return 0
-
-    def get_words_freq(self):
-        return 0
-
-    def get_likelihood(self):
-        return 0
-
     def calculate_w(self, z: np.ndarray):
         w_matrix = np.ndarray((self.num_topics, self.num_docs), dtype='float64')
 
@@ -96,15 +92,15 @@ class Em:
             w_matrix[:, t] = w_matrix[:, t] / all_not_zero
         return w_matrix
 
-
     def initialize_alpha(self, num_topics):
         return np.array([1/num_topics]*num_topics)
 
-    def calculate_p(self, num_topics):
+    def calculate_p(self, num_topics, w):
+        start = time()
         p_matrix = np.ndarray((num_topics, len(self.words_stat)), dtype='float64')
 
         # for each topic
-        for i in num_topics:
+        for i in range(num_topics):
 
             sum_numerator = 0
             sum_denominator = 0
@@ -115,22 +111,22 @@ class Em:
                 # for each document
                 for t, doc in enumerate(self.data):
                     n_t_k = self.get_n_t_k(doc, word)
-                    sum_numerator += self.w[i][t]*n_t_k
-                    sum_denominator += self.w[i][t]*doc.len
+                    sum_numerator += w[i][t]*n_t_k
+                    sum_denominator += w[i][t]*doc.len
 
                 sum_numerator += self.LAMBDA
                 sum_denominator += self.LAMBDA*len(self.words_stat)
 
                 p_matrix[i][k] = sum_numerator/sum_denominator
-
+        logger.info(f"time calculate p: {time() - start}")
         return p_matrix
 
-    def calculate_alpha(self):
+    def calculate_alpha(self, w):
         alpha = np.zeros(len(self.topic2i))
         for i in range(len(self.topic2i)):
-            print(f"topic line length: {len(self.w[i, :])}")
+            print(f"topic line length: {len(w[i, :])}")
 
-            alpha_value = np.sum(self.w[i, :])
+            alpha_value = np.sum(w[i, :])
             if alpha_value == 0:
                 alpha_value = self.EPSILON
 
@@ -172,13 +168,52 @@ class Em:
         return n_t_k
 
     def run_em(self):
-        
-        return 0
+        logger.info("start em")
+        w = self.initialize_w(self.num_topics, self.num_docs)
+        logger.info("finish initialize w")
+        alpha = self.initialize_alpha(self.num_topics)
+        logger.info("finish initialize alph")
+        p = self.calculate_p(self.num_topics, w)
+        logger.info("finish initialize p")
+
+        for i in range(3):
+            logger.info(f"start iteration number {str(i)}")
+            # E
+            z = self.calculate_z(alpha, p)
+            logger.info("finish calculate z")
+            w = self.calculate_w(z)
+            logger.info("finish calculate w")
+
+            # M
+            alpha = self.calculate_alpha(w)
+            logger.info("finish calculate alpha")
+            p = self.calculate_p(self.num_topics, w)
+            logger.info("finish calculate p")
+
+    def get_likelihood(self, z):
+        sum_l = 0
+        for t in range(len(self.data)):
+
+            m_t = z[:, t].max()
+            sum_ln = 0
+            for i in range(self.num_topics):
+                if z[i][t] - m_t >= - self.K:
+                    sum_ln += e**(z[i][t] - m_t)
+
+            sum_l += m_t + np.log(sum_ln)
+
+        return sum_l
+
+    def get_clusters(self):
+        pass
+
 
 def main():
     data_path = sys.argv[1]
     topic_path = sys.argv[2]
     algo = Em(data_path, topic_path)
+    algo.run_em()
+
 
 if __name__ == '__main__':
     main()
